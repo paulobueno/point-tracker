2d1e3d36764e64cb6c177f4845b4c5f970e1dbc2
Paulo Bueno <paulob.bruno@gmail.com>
2023-01-29 11:08:05 -0300

    refactoring static files to load from s3 or locally

--- point_tracker/settings.py
+++ point_tracker/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+if False:
+    STATIC_URL = '../static/'
+else:
+    STATIC_URL = 'https://footracker-statics.s3.sa-east-1.amazonaws.com/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
