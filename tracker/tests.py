4580c5bdbf51e2d064705df0ce29fa7b770a35b3
Paulo Bueno <paulob.bruno@gmail.com>
2023-07-04 23:40:56 -0300

    adding first unittests

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
+from tracker.views import teams
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolvers_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, teams)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = teams(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>FooTracker</title>', html)
+        self.assertTrue(html.startswith('<!doctype html>'))
+        self.assertTrue(html.endswith('</html>'))
